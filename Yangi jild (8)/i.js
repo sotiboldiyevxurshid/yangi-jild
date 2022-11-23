842491dd8e0458fa639193fe9db85615402b3c42
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2022-11-23 20:41:47 +0500

    ee

---
+++ Yangi jild (8)/i.js
@@ -0,0 +1,50 @@
+// let a = +prompt("raqam")
+
+
+// for(let i = a;i <= a;i++){
+    
+//         console.log( i * 12);
+//         console.log(i * 56);
+        
+   
+
+// }
+
+
+
+// let b= +prompt("raqam")
+
+
+// for(let i = b;i <= b;i++){
+    
+//         console.log( i * 3);
+
+
+        
+        
+   
+
+// }
+
+
+let a = +prompt("raqam")
+
+
+for(let i = a;i <= a;i++){
+
+    if(i % 2 == 0){
+        console.log("juft",i);
+    }
+        else{
+            console.log("toq",i);
+        }
+    }
+    
+       
+
+        
+        
+        
+   
+
+
